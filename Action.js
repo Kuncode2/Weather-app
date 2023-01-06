a405389bcbfa380eedf8b80e6e22aecfd5a6a0f3
kunal <kuncode2@gmail.com>
2023-01-06 22:29:10 +0530

    js

--- Action.js
+++ Action.js
@@ -1,3 +1,16 @@
 let weather = {
-    "apikey": "f411c6db65ce29ef7dcc057b4e54a39f"
+    apikey: "f411c6db65ce29ef7dcc057b4e54a39f",
+    fetchWeather: function (city){
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
+        +city 
+        + "&units=metric&appid=" 
+        + this.apikey)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+        
+    },
+    displayWeather: function(data) {
+        
+
+    }
 }
